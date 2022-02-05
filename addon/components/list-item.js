38bfde2243209ddffbd3a8f170d8803e8502183e
Ed Graham <ed.graham2@gmail.com>
2022-02-05 17:31:22 +0000

    smoother

--- addon/components/list-item.js
+++ addon/components/list-item.js
@@ -1,20 +1,14 @@
-// import { guidFor } from '@ember/object/internals';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { next } from '@ember/runloop';
 
 export default class ListItem extends Component {
-  // @service rightClickMenu;
-
-  // popperId = guidFor(this);
   @tracked show_sub_menu = false
   sub_menu_element = null
 
   @action
   saveElement(element) {
-    // console.log('saveElement', element)
     this.sub_menu_element = element
   }
 
@@ -26,7 +20,6 @@ export default class ListItem extends Component {
       return;
     }
 
-    // console.log('this.sub_menu_element', this.sub_menu_element)
     if(!this.sub_menu_element) return
     this.show_sub_menu = true
 
@@ -34,36 +27,27 @@ export default class ListItem extends Component {
     
     let x = element.offsetWidth - 5;
     let y = element.offsetY;
-    // console.log('element', rect.left, element.offsetWidth)
 
+    this.sub_menu_element.style.visibility = 'hidden'
     this.sub_menu_element.style.left = x + 'px'
     this.sub_menu_element.style.top = y + 'px'
 
-    // let rect = e.target.getBoundingClientRect()
-    // console.log('rect', rect.right, this.sub_menu_element.offsetWidth)
-    // let right = rect.right + this.sub_menu_element.offsetWidth
-    // let bottom = rect.bottom + this.sub_menu_element.offsetHeight
     next(() => {
       let rect = this.sub_menu_element.getBoundingClientRect()
       let right = rect.right
       let bottom = rect.bottom
-      // console.log('next', right)
 
-      // console.log(y, this.element.offsetHeight, window.innerHeight)
       if(bottom > window.innerHeight){
         this.sub_menu_element.style.bottom = '0px'
         this.sub_menu_element.style.top = ''
       }
       
-      // console.log('right', right, window.innerWidth)
       if(right > window.innerWidth){
         this.sub_menu_element.style.left = - this.sub_menu_element.offsetWidth + 'px'
-        // this.sub_menu_element.style.left = ''
-        // this.sub_menu_element.style.left = (x - this.sub_menu_element.offsetWidth) + 'px'
       }
+      this.sub_menu_element.style.visibility = 'visible'
     })
     
-    // this.rightClickMenu.createPopper(`popper-for-${this.popperId}`);
   }
 
   @action
--- addon/components/right-click-menu.js
+++ addon/components/right-click-menu.js
@@ -1,63 +1,27 @@
-// import { guidFor } from '@ember/object/internals';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 import { next } from '@ember/runloop';
 
 export default class RightClickMenuComponent extends Component {
-  @service rightClickMenu;
-
   @tracked show = false
-
-  // popperId = guidFor(this);
-
-  // generateGetBoundingClientRect(x = 0, y = 0) {
-  //   return () => ({
-  //     width: 0,
-  //     height: 0,
-  //     top: y,
-  //     right: x,
-  //     bottom: y,
-  //     left: x,
-  //   });
-  // }
-
-  // @action
-  // getTargetElement(popperId) {
-  //   let tetherElement = document.querySelector(`#tether-for-${popperId}`);
-  //   let targetElement = tetherElement && tetherElement.parentElement;
-
-  //   if (targetElement) {
-  //     tetherElement.remove();
-  //     this.targetElement = targetElement;
-  //     return targetElement;
-  //   }
-  // }
 
   @action
   addContextMenuListeners(element) {
     window.addEventListener('click', this.closeContextMenu);
     window.addEventListener('contextmenu', this.closeContextMenu);
 
-    // console.log('addContextMenuListeners', element, element.parentElement)
     this.triggerElement = element.parentElement
     this.triggerElement.addEventListener(
       'contextmenu',
       this.contextMenu
     );
-
-    // this.getTargetElement(this.popperId).addEventListener(
-    //   'contextmenu',
-    //   this.contextMenu
-    // );
   }
 
   @action
   saveElement(element) {
     this.element = element
-
   }
 
   @action
@@ -74,15 +38,12 @@ export default class RightClickMenuComponent extends Component {
 
   @action
   contextMenu(e) {
-    console.log('contextMenu')
     e.preventDefault();
     this.show = true
-    // let popperElementId = `popper-for-${this.popperId}`;
 
-    // let { clientX: x, clientY: y } = e;
     let { pageX: x, pageY: y } = e;
-    // console.log(x, y, this.element, this.element.offsetHeight)
 
+    this.element.style.visibility = 'hidden'
     this.element.style.left = x + 'px'
     this.element.style.top = y + 'px'
 
@@ -94,34 +55,13 @@ export default class RightClickMenuComponent extends Component {
       if((x + this.element.offsetWidth) > window.innerWidth){
         this.element.style.left = (x - this.element.offsetWidth) + 'px'
       }
+      this.element.style.visibility = 'visible'
     })
-
-    
-
-    // let virtualElement = {
-    //   getBoundingClientRect: () => {
-    //     return {
-    //       width: 0,
-    //       height: 0,
-    //       top: y,
-    //       right: x + 10,
-    //       bottom: y,
-    //       left: x,
-    //     };
-    //   },
-    // };
-
-    // this.rightClickMenu.createPopper(
-    //   popperElementId,
-    //   this.targetElement,
-    //   virtualElement
-    // );
   }
 
   @action
   closeContextMenu(e) {
-    // console.log('closeContextMenu', this.element, e.target)
-    if (this.triggerElement.contains(e.target)) return
+    if (e && this.triggerElement.contains(e.target)) return
 
     if (
       e &&
@@ -139,7 +79,6 @@ export default class RightClickMenuComponent extends Component {
 
     if (!e || e.type === 'click' || !e.path.includes(this.targetElement)) {
       this.show = false
-      // this.rightClickMenu.closePopper(this.targetElement);
     }
   }
 }
